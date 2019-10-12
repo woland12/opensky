8f7c0ebd8a145328ea2f5462e8fdb19269ee3a83
Pavel Murzintsev <murzintsev.p@gmail.com>
2019-10-12 09:52:19 +0300

    добавил переход по страницам /my-route?page=2

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from get_plane_states import get_states
+   
+app = Flask(__name__)
+@app.route("/")
+def index():
+    title = 'Состояния самолетов мира'
+    list_states = get_states()
 
-def create_app():
-    app = Flask(__name__)
-    @app.route("/")
-    def index():
-        title = 'Состояния самолетов мира'
-        list_states = get_states()
-        return render_template('index.html',page_title = title,list_states = list_states)
+    number_of_page = 1
+    num_of_lines_page = 50
+    num_start = (number_of_page-1)*num_of_lines_page
+    num_finish = number_of_page*num_of_lines_page-1
+    list_of_entries = range(num_start,num_finish)
 
-    return app
+    return render_template('index.html',
+                            page_title = title,
+                            list_states = list_states,
+                            number_of_page = number_of_page,
+                            list_of_entries=list_of_entries)
+
+
+@app.route('/my-route')
+def my_route():
+    page = request.args.get('page', default = 1, type = int)
+    filter = request.args.get('filter', default = '*', type = str)
+    title = 'Состояния самолетов мира'
+    list_states = get_states()
+    
+    num_of_lines_page = 50
+    number_of_page = min(page,len(list_states)//num_of_lines_page)
+    num_start = (number_of_page-1)*num_of_lines_page
+    num_finish = number_of_page*num_of_lines_page-1
+    list_of_entries = range(num_start,num_finish)
+
+    return render_template('index.html',
+                            page_title = title,
+                            list_states = list_states,
+                            number_of_page = number_of_page,
+                            list_of_entries=list_of_entries)
