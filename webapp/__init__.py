aa6bba3ba99ffd5fb5972a560d4b2fbd3f8f7751
Pavel Murzintsev <murzintsev.p@gmail.com>
2019-10-13 19:34:40 +0300

    Ограничил число записей на страницу 20. Добавил кнопки перехода по страницам

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,33 +8,37 @@ def index():
     list_states = get_states()
 
     number_of_page = 1
-    num_of_lines_page = 50
+    num_of_lines_page = 20
     num_start = (number_of_page-1)*num_of_lines_page
     num_finish = number_of_page*num_of_lines_page-1
     list_of_entries = range(num_start,num_finish)
+    len_list_of_entries = len(list_states)//num_of_lines_page
 
     return render_template('index.html',
                             page_title = title,
                             list_states = list_states,
                             number_of_page = number_of_page,
-                            list_of_entries=list_of_entries)
+                            list_of_entries=list_of_entries,
+                            len_list_of_entries=len_list_of_entries)
 
 
 @app.route('/my-route')
 def my_route():
     page = request.args.get('page', default = 1, type = int)
-    filter = request.args.get('filter', default = '*', type = str)
+    #filter = request.args.get('filter', default = '*', type = str)
     title = 'Состояния самолетов мира'
     list_states = get_states()
     
-    num_of_lines_page = 50
+    num_of_lines_page = 20
     number_of_page = min(page,len(list_states)//num_of_lines_page)
     num_start = (number_of_page-1)*num_of_lines_page
     num_finish = number_of_page*num_of_lines_page-1
     list_of_entries = range(num_start,num_finish)
+    len_list_of_entries = len(list_states)//num_of_lines_page
 
     return render_template('index.html',
                             page_title = title,
                             list_states = list_states,
                             number_of_page = number_of_page,
-                            list_of_entries=list_of_entries)
+                            list_of_entries=list_of_entries,
+                            len_list_of_entries=len_list_of_entries)
